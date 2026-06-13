ec924df387fd54b8da80643c170f64c0fe24fe1e
osnailcyargta-ctrl <osnail.cyargta@gmail.com>
2026-06-13 18:23:27 +0700

    Update MainActivity.kt

--- app/src/main/java/com/asepstore/app/MainActivity.kt
+++ app/src/main/java/com/asepstore/app/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.asepstore.app
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: AppStoreAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +15,17 @@ class MainActivity : AppCompatActivity() {
 
         val recycler = findViewById<RecyclerView>(R.id.recycler)
         recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = AppStoreAdapter(AppRepository.apps, this)
+        adapter = AppStoreAdapter(AppRepository.apps, this)
+        recycler.adapter = adapter
+    }
+
+    fun refreshList() {
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Refresh installed state when returning from system uninstall/install
+        adapter.notifyDataSetChanged()
     }
 }
