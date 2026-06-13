ab6aa593d39c6d4ad4dd5c9990de573a5480410a
osnailcyargta-ctrl <osnail.cyargta@gmail.com>
2026-06-13 17:27:27 +0700

    Add app/src/main/java/com/asepstore/app/MainActivity.kt

---
+++ app/src/main/java/com/asepstore/app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.asepstore.app
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = AppStoreAdapter(AppRepository.apps, this)
+    }
+}
