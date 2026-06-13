6559292c6d57cab6c7f65b98cd57c44577f30837
osnailcyargta-ctrl <osnail.cyargta@gmail.com>
2026-06-13 17:26:46 +0700

    Add app/src/main/java/com/asepstore/app/AppItem.kt

---
+++ app/src/main/java/com/asepstore/app/AppItem.kt
@@ -0,0 +1,13 @@
+package com.asepstore.app
+
+data class AppItem(
+    val id: Int,
+    val name: String,
+    val shortDesc: String,
+    val fullDesc: String,
+    val size: String,
+    val version: String,
+    val emoji: String,
+    val apkUrl: String,
+    val features: List<String>
+)
